0e52d45873cd83d671675295a426ceb1204e55a1
Matt Botz and J. Ryan Conklin <jryanconklin@gmail.com>
2016-06-20 19:24:56 -0700

    Add third spec from BDD, evaluate whether number entered is divisible by 100.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 // business logic
 var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
     return true;
   } else {
     return false;
-  }  
+  }
 };
 
 // user interface logic
